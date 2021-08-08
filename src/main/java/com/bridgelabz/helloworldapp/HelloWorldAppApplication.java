38482b4aeaa09d181dc398b68ee827741e97544d
DivyaMundhada <divyamundada9@gmail.com>
2021-08-08 20:25:21 +0530

    [Divya] Added: HelloWorld Application with STS.

---
+++ src/main/java/com/bridgelabz/helloworldapp/HelloWorldAppApplication.java
@@ -0,0 +1,19 @@
+/**
+ * Creating a HelloWorld Application using STS.
+ * 
+ * @author DIVYA MUNDHADA
+ * @since 08.08.2021
+ */
+package com.bridgelabz.helloworldapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldAppApplication.class, args);
+	}
+
+}
